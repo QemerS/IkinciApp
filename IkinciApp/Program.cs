f38fbd4e1f4ab460ec173750ecda7468e0b962a5
QemerS <tu201806169@code.edu.az>
2021-10-26 02:45:23 +0400

    Add project files.

---
+++ IkinciApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IkinciApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 - dən 100 - ə qədər tək ədədləri console-da çap edin;
+            for (short x = 1; x <= 100; x++)
+            {
+                if (x % 2 != 0)
+                {
+                    Console.WriteLine(x);
+                }
+
+            }
+        }
+    }
+}
